e451afc1202852aaf8a9bb21c4adcc48fac58a86
JOSHUA TAYLOR <joshuatlr418@gmail.com>
2015-11-18 08:07:03 -1000

    added pages
    Added skeletal pages for each option on the home page and created the
    links in the router. Working on login page

--- app/lib/router/Router.js
+++ app/lib/router/Router.js
@@ -27,6 +27,28 @@ Router.route('/textbook/:_id', {
   name: 'EditTextbook',
   data: function() { return Textbook.findOne(this.params._id); }
 });
+
+Router.route('/students', {
+  name: 'Students'
+});
+
+Router.route('/textbooks', {
+  name: 'Textbooks'
+});
+
+Router.route('/random1', {
+  name: 'random1'
+});
+
+Router.route('/random2', {
+  name: 'random2'
+});
+
+Router.route('/matches', {
+  name: 'Matches'
+});
+
 Router.route('/help', {
   name: 'Help'
 });
+
