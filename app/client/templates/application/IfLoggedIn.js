bb1f6bdb71f30255caeb88f27e7cb14ac9554bef
mike31s <michael88spencer@gmail.com>
2015-11-21 13:55:44 -1000

    Mike - working on profiles and setting up admin roles

--- app/client/templates/application/IfLoggedIn.js
+++ app/client/templates/application/IfLoggedIn.js
@@ -1,14 +1,28 @@
+var checked = false;
 Template.IfLoggedIn.helpers({
   /**
    * @returns {*} True if Meteor is in the process of logging in.
    */
-  authInProcess: function() {
+  authInProcess: function () {
     return Meteor.loggingIn();
   },
   /**
    * @returns {boolean} True if there is a logged in user.
    */
-  canShow: function() {
+
+  canShow: function () {
+
+    if (!checked) {
+      console.log(Meteor.user());
+      //console.log(Meteor.settings.public.adminUsers);
+/*
+      if (_.contains((Meteor.settings.public.adminUsers), Meteor.user()._id))
+      //  while(!(Roles.addUsersToRoles(Meteor.user(), ['admin'])));
+        Roles.addUsersToRoles(Meteor.user(), ['admin'])*/
+      checked = true;
+    }
+
     return !!Meteor.user();
+
   }
 });
--- app/server/seeds/Accounts.js
+++ app/server/seeds/Accounts.js
@@ -1,4 +1,8 @@
-/* Validate username, sending a specific error message on failure. */
+/* Validate username, sending a specific error message on failure.
+* this part is buggy, will leave it commented out until solution is found
+* */
+
+/*
 Accounts.validateNewUser(function (user) {
   if (user) {
     var username = user.services.cas.id;
@@ -7,4 +11,19 @@ Accounts.validateNewUser(function (user) {
     }
   }
   throw new Meteor.Error(403, "User not in the allowed list");
-});
+});*/
+
+console.log(Meteor.users.find().fetch());
+ // Roles.addUsersToRoles('michael4', ['admin'])
+
+console.log("*****");
+//Roles.addUsersToRoles('rFvTJB5eY2Ep5dZio', ['admin'])
+
+//if (_.contains((Meteor.settings.public.adminUsers), Meteor.user()._id))
+//  while(!(Roles.addUsersToRoles(Meteor.user(), ['admin'])));
+//  Roles.addUsersToRoles(Meteor.user(), ['admin'])
+
+for(var i = 0; i<Meteor.settings.adminUsers.length; i++)
+  Roles.addUsersToRoles(Meteor.settings.adminUsers[i], ['admin'])
+
+
