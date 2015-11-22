faa72a30d72b112763c730e50edcbe6753b75cde
skchun <skchun@hawaii.edu>
2015-11-21 15:56:13 -1000

    #18 Added textbook conditions

--- app/lib/collections/Textbook.js
+++ app/lib/collections/Textbook.js
@@ -60,10 +60,28 @@ Textbook.attachSchema(new SimpleSchema({
       placeholder: "Author"
     }
   },
+  condition: {
+    label: "Condition",
+    type: String,
+    allowedValues: ['Excellent', 'Good', 'Fair', 'Poor'],
+    optional: false,
+    max: 100,
+    autoform: {
+      group: textbook,
+      placeholder: "Condition",
+      options:[
+        {label: "Excellent", value: "Excellent"},
+        {label: "Good", value: "Good"},
+        {label: "Fair", value: "Fair"},
+        {label: "Poor", value: "Poor"}
+      ]
+    }
+  },
   isbn: {
     label: "ISBN",
     type: String,
     optional: false,
+    min: 10,
     max: 10,
     autoform: {
       group: textbook,
