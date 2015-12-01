41bc2ff22e8da2dbad115439c32b3dd7ca0cefbc
JOSHUA TAYLOR <joshuatlr418@gmail.com>
2015-11-30 20:10:11 -1000

    Student Stuff added

---
+++ app/client/templates/stuff/student/ListStudent.js
@@ -0,0 +1,24 @@
+/**
+ * Created by jtayl_000 on 11/30/2015.
+ */
+Template.ListStudent.helpers({
+
+  /**
+   * @returns {*} All of the BuyOffer documents.
+   */
+  studentList: function () {
+    return Student.find();
+  }
+});
+
+Template.ListStudent.events({
+  'click .deletestudent': function(e) {
+    e.preventDefault();
+
+    if (confirm("Delete this post?")) {
+      var currentPostId = this._id;
+      Meteor.call("deleteStudent", currentPostId);
+      Router.go('ListStudent');
+    }
+  }
+});
