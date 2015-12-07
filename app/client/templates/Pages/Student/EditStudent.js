1d960a3c6c6b69573d0d83c578fc2c8c7e7e4ba4
JOSHUA TAYLOR <joshuatlr418@gmail.com>
2015-12-06 18:05:23 -1000

    Working December 6 part 4

--- app/client/templates/Pages/Student/EditStudent.js
+++ app/client/templates/Pages/Student/EditStudent.js
@@ -5,13 +5,10 @@
  */
 AutoForm.hooks({
   EditStudentForm: {
-    /**
-     * After successful form submission, go to the ListFlight page.
-     * @param formType The form.
-     * @param result The result of form submission.
-     */
+
     onSuccess: function(formType, result) {
-      Router.go('Home');
+      if(confirm("Submit?")){
+      Router.go('Home');}
     }
   }
 });
