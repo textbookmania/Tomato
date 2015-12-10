af5109e9a21389ec544a0cfa2a3d290e1526f1b5
Gilbert Yim <gyim@hawaii.edu>
2015-12-09 19:02:41 -1000

    Incomplete for now

---
+++ app/client/templates/Pages/Matches.js
@@ -0,0 +1,57 @@
+
+Template.Matches.helpers({
+
+    /**
+     * @returns {*} All non-expired matches for current users sell offers.
+     */
+    findSellMatches: function () {
+        var sellOfferMatches=[];
+        var buyOfferCursor =[];
+        var mySellOffer = SellOffer.find({ owner: Meteor.user().profile.name }).fetch();
+        _.each(mySellOffer, function(rec) {
+            buyOfferCursor = buyOfferCursor.concat(BuyOffer.find({book: rec.book}));
+        });
+        _.each(buyOfferCursor, function(curse) {
+            curse.forEach(function (offer) {
+                if (moment(offer.expires).isAfter()) {
+                    sellOfferMatches = sellOfferMatches.concat(offer);
+                }
+            });
+        });
+        return sellOfferMatches;
+    },
+
+    /**
+     * @returns {*} All non-expired matches for current users buy offers.
+     */
+    findBuyMatches: function () {
+        var buyOfferMatches=[];
+        var sellOfferCursor =[];
+        var myBuyOffer = BuyOffer.find({owner: Meteor.user().profile.name}).fetch();
+        _.each(myBuyOffer, function(record){
+            sellOfferCursor = sellOfferCursor.concat(SellOffer.find({book: record.book}));
+        });
+        _.each(sellOfferCursor, function(curse) {
+            curse.forEach(function (offer) {
+                if (moment(offer.expires).isAfter()) {
+                    buyOfferMatches = buyOfferMatches.concat(offer);
+                }
+            });
+        });
+        return buyOfferMatches;
+    }
+});
+
+Template.Matches.events({
+    'click .accept': function(e){
+        e.preventDefault();
+        if (confirm("Accept this offer??")) {
+            var docId = this._id;
+            this.expires=moment().format();
+            this.seller=Meteor.user().profile.name;
+            var doc = this;
+            Meteor.call("contactOfferOwner", docId, doc);
+            //Router.go('Matches');
+        }
+    }
+});
