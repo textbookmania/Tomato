6e4041a1a6d76966e56aa477e1d27824bd66e2e0
skchun <skchun@hawaii.edu>
2015-12-13 20:14:11 -1000

    #49 Added responsive carousel to home page

---
+++ app/client/templates/application/Carousel.js
@@ -0,0 +1,14 @@
+Template.carousel.rendered = function() {
+  $("#owl-demo").owlCarousel({
+
+    navigation : true, // Show next and prev buttons
+    slideSpeed : 1000,
+    paginationSpeed : 400,
+    rewindSpeed : 1000,
+    rewindNav : true,
+    autoplay : 2500,
+    items : 1,
+    loop: true
+
+  });
+};
