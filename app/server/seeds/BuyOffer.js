9a3ee1f4bcf5aafd77fab3ad7537dc438895b39d
skchun <skchun@hawaii.edu>
2015-12-14 17:02:06 -1000

    #44 Updated seeds

--- app/server/seeds/BuyOffer.js
+++ app/server/seeds/BuyOffer.js
@@ -1,7 +1,7 @@
 var buyOfferSeeds = [
-    {book: "Python Programming: An Introduction to Computer Science", condition: "good",offer: 1,owner:"0", expires: moment().subtract(1,'days').format()},
-    {book: "Java Concepts: Compatible with Java 5, 6 and 7", condition: "poor",offer: 2,owner:"1", expires: moment().format()},
-    {book: "Discrete Mathematics and Its Applications Seventh Edition", condition: "poor",offer: 5, owner: "2", expires: moment().add(1,'years').format()}
+    {book: "Python Programming: An Introduction to Computer Science", condition: "Good",offer: 1,owner:"0", expires: moment().subtract(1,'days').format()},
+    {book: "Java Concepts: Compatible with Java 5, 6 and 7", condition: "Poor",offer: 2,owner:"1", expires: moment().format()},
+    {book: "Discrete Mathematics and Its Applications Seventh Edition", condition: "Don't care",offer: 5, owner: "2", expires: moment().add(1,'years').format()}
 ];
 
 if (BuyOffer.find().count() === 0) {
--- app/server/seeds/SellOffer.js
+++ app/server/seeds/SellOffer.js
@@ -1,7 +1,7 @@
 var sellOfferSeeds = [
-    {book: "Data Structures: Abstraction and Design Using Java", condition: "good",offer: 1, owner: "3", expires: moment().subtract(1,'days').format()},
-    {book: "The C Programming Language", condition: "poor",offer: 2, owner: "4", expires: moment().format()},
-    {book: "C++ Primer Plus (6th Edition) (Developer's Library)", condition: "poor",offer: 5, owner: "5", expires: moment().add(1,'years').format()}
+    {book: "Data Structures: Abstraction and Design Using Java", condition: "Good",offer: 1, owner: "3", expires: moment().subtract(1,'days').format()},
+    {book: "The C Programming Language", condition: "Poor",offer: 2, owner: "4", expires: moment().format()},
+    {book: "C++ Primer Plus (6th Edition) (Developer's Library)", condition: "Don't care",offer: 5, owner: "5", expires: moment().add(1,'years').format()}
 ];
 
 if (SellOffer.find().count() === 0) {
