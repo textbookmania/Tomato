f58dd9ace3d7702340661d9c1a3818805d5491f1
JOSHUA TAYLOR <joshuatlr418@gmail.com>
2015-12-15 01:23:20 -1000

    basic matching done

--- app/client/templates/Pages/Matches/ListMatches.js
+++ app/client/templates/Pages/Matches/ListMatches.js
@@ -2,34 +2,24 @@ Template.ListMatches.helpers({
 
   sellOfferList: function () {
     var sellOfferMatches = [];
-    var mySellOffers = SellOffer.find({owner: Meteor.user().profile.name});
-    var allBuys = BuyOffer.find();
+    var buyOfferCursor = [];
+    var mySellOffer = [];
+    var mySellOfferCurse = SellOffer.find({owner: Meteor.user().profile.name});
 
-
-
-
-
-    /*
-    var mySellOfferCurse = SellOffer.find({ owner: Meteor.user().profile.name });
-
-    mySellOfferCurse.forEach(function(foo){
-      if (moment(foo.expires).isAfter()) {
+    mySellOfferCurse.forEach(function (foo) {
         mySellOffer = mySellOffer.concat(foo);
-      }
     });
 
-    _.each(mySellOffer, function(rec) {
-      buyOfferCursor = buyOfferCursor.concat(BuyOffer.find({owner: rec.book}));
+    _.each(mySellOffer, function (record) {
+      buyOfferCursor = buyOfferCursor.concat(BuyOffer.find({book: record.book}));
     });
 
-    _.each(buyOfferCursor, function(curse) {
+    _.each(buyOfferCursor, function (curse) {
       curse.forEach(function (offer) {
-        if (moment(offer.expires).isAfter()) {
           sellOfferMatches = sellOfferMatches.concat(offer);
-        }
       });
-    });*/
-    return mySellOffers;
+    });
+    return sellOfferMatches;
   },
 
   fullList: function () {
@@ -48,17 +38,17 @@ Template.ListMatches.helpers({
 
 
     //for each does the same thing for each element in array
-    myBuyOfferCurse.forEach(function (foo){
+    myBuyOfferCurse.forEach(function (foo) {
       myBuyOffer = myBuyOffer.concat(foo);
     });
 
-       _.each(myBuyOffer, function(record){
+    _.each(myBuyOffer, function (record) {
       sellOfferCursor = sellOfferCursor.concat(SellOffer.find({book: record.book}));
     });
 
-    _.each(sellOfferCursor, function(curse) {
+    _.each(sellOfferCursor, function (curse) {
       curse.forEach(function (offer) {
-          buyOfferMatches = buyOfferMatches.concat(offer);
+        buyOfferMatches = buyOfferMatches.concat(offer);
       });
     });
 
@@ -67,15 +57,13 @@ Template.ListMatches.helpers({
 });
 
 Template.ListMatches.events({
-  'click .accept': function(e){
+  'click .accept': function (e) {
     e.preventDefault();
-    if (confirm("Accept this offer??")) {
-      var docId = this._id;
-      this.expires=moment().format();
-      this.seller=Meteor.user().profile.name;
-      var doc = this;
-      Meteor.call("contactOfferOwner", docId, doc);
-      //Router.go('Matches');
-    }
+    confirm("Accept this offer?");
+  },
+
+  'click .contact': function (e) {
+    e.preventDefault();
+    confirm("Contact Seller?");
   }
 });
